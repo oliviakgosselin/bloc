bdfd700d324af20af6ce5e15f3fbe4f1e5d46c21
Chris Grant <chris@chrisgrant.nyc>
2019-08-14 16:59:20 -0400

    favicon

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.locals.ENV_DEVELOPMENT = env == 'development';
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
-// app.set('view engine', 'handlebars');
 
 app.use(favicon(path.join(__dirname, 'public/img/favicons/favicon.ico')));
 app.use(logger('dev'));
